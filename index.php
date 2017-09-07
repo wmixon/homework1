4107e145391f18e0b9c91cdcfcbeb0a1f454deb2
Westley Mixon <wmixon@csumb.edu>
2017-09-07 06:06:55 +0000

    Working 4 page site

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="./style.css" rel="stylesheet" type="text/css" />
+        <link rel="icon" href="002-home.png">
+        <title>Home</title>
+    </head>
+    <h1> AEON</h1>
+    <nav>
+        <a href="index.php" id="current">Home</a>
+        <a href="bitcoins.html">Bitcoins</a>
+        <a href="why.html">Why?</a>
+        <a href="how.html">Where to start</a>
+    </nav>
+    <div class="head1">
+    <p>
+        AEON isn't just a currency.
+    </p>
+    
+    <p>
+        It's a lifestyle.
+    </p>
+    </div>
+    <div id="aeon">
+        <img src="aeon.png" alt="Aeon Logo"/>
+    </div>
+            <div class="head2">
+        <p>Aeon is a curreny that wil change the way that people think about money.</p>
+        <p>A changing currency is slowly becoming more accepted each day.</p>
+        <p>Making the switch has never been easier.</p>
+        <p>~</p>
+        <p>Aeon is allowing people to become and do things that they never expected.</p>
+        <p>Freedom is privacy.</p>
+        <p>Take control of privacy with</p>
+        <p>AEON</p>
+    </div>
+    <a href="http://www.aeon.cash/" class="source">Source</a>
+</html>
